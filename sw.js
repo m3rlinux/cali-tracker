56f1cdaa462d173d5254e973391c990af4d5ce36
Davide Gibilisco <m3rlinux.it@gmail.com>
2026-06-11 10:29:13 +0200

    v3.0.1: modifica emoji

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
   Released under the MIT License
   https://github.com/m3rlinux/cali-tracker
 */
-const CACHE_VERSION = '3.0.0';
+const CACHE_VERSION = '3.0.1';
 const CACHE_NAME = `cali-tracker-${CACHE_VERSION}`;
 const ASSETS = [
   './',
